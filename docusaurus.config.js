76db78120781af8b51630025f42146cafad98591
Space V <40030799+ahcenezdh@users.noreply.github.com>
2023-10-28 21:18:20 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,10 +11,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://ahcenezdh.github.io/scaleformui-docs/',
+  url: 'https://ahcenezdh.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/scaleformui-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
